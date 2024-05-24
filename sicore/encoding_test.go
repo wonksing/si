38ee2b672b4d5bdf1d96082f82672359c865146e
wonkdance <83893434+wonkdance@users.noreply.github.com>
2024-05-25 02:54:37 +0900

    test: Add test for EncodeJsonCopied (#17)
    Co-authored-by: wonksing <wonksing@gmail.com>

--- sicore/encoding_test.go
+++ sicore/encoding_test.go
@@ -20,6 +20,19 @@ func Test_EncodeJson(t *testing.T) {
 	require.EqualValues(t, b, dst.Bytes())
 }
 
+func Test_EncodeJsonCopied(t *testing.T) {
+	dst := bytes.Buffer{}
+	src := _testStruct{
+		Msg: "hello world",
+	}
+	copied, err := EncodeJsonCopied(&dst, &src)
+	require.Nil(t, err)
+	b, _ := json.Marshal(&src)
+	b = append(b, '\n')
+	require.EqualValues(t, b, dst.Bytes())
+	require.EqualValues(t, b, copied.Bytes())
+}
+
 type _testStruct struct {
 	Msg string `json:"msg"`
 }
