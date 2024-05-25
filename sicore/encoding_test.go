a18de1f53da9ed688ae7136bd6a2fe4be06d8203
wonksing <wonksing@gmail.com>
2024-05-25 10:22:22 +0900

    test: Add tests to encoding

--- sicore/encoding_test.go
+++ sicore/encoding_test.go
@@ -33,6 +33,90 @@ func Test_EncodeJsonCopied(t *testing.T) {
 	require.EqualValues(t, b, copied.Bytes())
 }
 
+func Test_HmacSha256HexEncoded(t *testing.T) {
+	res, err := HmacSha256HexEncoded("asdf", []byte("hello world"))
+	require.Nil(t, err)
+	require.EqualValues(t, "2c78fedf60d1f955bf0c9e14ed6b332a6efb6e5668fc6aa067257558cdbb7d6d", res)
+}
+
+func Test_HmacSha256HexEncodedWithReader(t *testing.T) {
+	r := bytes.NewBufferString("hello world")
+	res, err := HmacSha256HexEncodedWithReader("asdf", r)
+	require.Nil(t, err)
+	require.EqualValues(t, "2c78fedf60d1f955bf0c9e14ed6b332a6efb6e5668fc6aa067257558cdbb7d6d", res)
+}
+
+func TestDefaultEncoder_Reset(t *testing.T) {
+	b := &bytes.Buffer{}
+	e := DefaultEncoder{}
+
+	e.Reset(b)
+	require.Equal(t, b, e.w)
+}
+
+func TestDefaultEncoder_Encode(t *testing.T) {
+
+	t.Run("succeed-bytes", func(t *testing.T) {
+		b := &bytes.Buffer{}
+		e := DefaultEncoder{
+			w: b,
+		}
+
+		data := []byte("hello")
+		err := e.Encode(data)
+		require.Nil(t, err)
+
+		require.EqualValues(t, data, b.Bytes())
+	})
+
+	t.Run("succeed-bytes_ptr", func(t *testing.T) {
+		b := &bytes.Buffer{}
+		e := DefaultEncoder{
+			w: b,
+		}
+
+		data := []byte("hello")
+		err := e.Encode(&data)
+		require.Nil(t, err)
+
+		require.EqualValues(t, data, b.Bytes())
+	})
+	t.Run("succeed-string", func(t *testing.T) {
+		b := &bytes.Buffer{}
+		e := DefaultEncoder{
+			w: b,
+		}
+
+		data := "hello"
+		err := e.Encode(data)
+		require.Nil(t, err)
+
+		require.EqualValues(t, data, b.String())
+	})
+	t.Run("succeed-string_ptr", func(t *testing.T) {
+		b := &bytes.Buffer{}
+		e := DefaultEncoder{
+			w: b,
+		}
+
+		data := "hello"
+		err := e.Encode(&data)
+		require.Nil(t, err)
+
+		require.EqualValues(t, data, b.String())
+	})
+	t.Run("fail", func(t *testing.T) {
+		b := &bytes.Buffer{}
+		e := DefaultEncoder{
+			w: b,
+		}
+
+		data := 1535
+		err := e.Encode(&data)
+		require.NotNil(t, err)
+	})
+}
+
 type _testStruct struct {
 	Msg string `json:"msg"`
 }
